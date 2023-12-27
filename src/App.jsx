2fae9eb340aebe9adc5e8c21b781c5b6556f4690
kratikesh18 <im.kartikesh18@gmail.com>
2023-12-27 08:42:19 +0530

    Modified some styles

--- src/App.jsx
+++ src/App.jsx
@@ -40,14 +40,15 @@ useEffect(()=>{
   localStorage.setItem('todos' , JSON.stringify(todos))
 } , [todos])
 
-
+// background: rgb(107,10,10);
+//background: linear-gradient(324deg, rgba(107,10,10,1) 0%, rgba(73,11,108,1) 92%);
 
   return (
     <TodoProvider value={{todos, toggleComplete,addTodo, updateTodo,deleteTodo}}>
 
-      <div className="bg-[#172842] min-h-screen w-full py-8">
-        <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
-          <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
+      <div className="bg-gradient-custom  min-h-screen w-full py-8">
+        <div className="w-full max-w-2xl mx-auto shadow-lg rounded-lg px-4 py-3 text-white">
+          <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Tasks</h1>
           <div className="mb-4">
             {/* Todo form goes here */}
             <TodoForm/>
